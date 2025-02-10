6345ea903d2481ea0894d33903788abdc6a3e2e7
Pia <piag.velasquez@outlook.com>
2025-02-10 19:47:12 -0300

    added geolocation

--- 19-webcam/scripts.js
+++ 19-webcam/scripts.js
@@ -45,7 +45,7 @@ function takePhoto() {
   const data = canvas.toDataURL("image/jpeg");
   const link = document.createElement("a");
   link.href = data;
-  link.setAttribute("download", "handsome");
+  link.setAttribute("download", "photo");
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
 }
